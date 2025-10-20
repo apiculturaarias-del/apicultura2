5178fdabf2d5d0404d3a8a24f4be70e35e24260d
apiculturaarias-del <apiculturaarias@gmail.com>
2025-10-20 17:49:56 +0000

    render

--- render_build.sh
+++ render_build.sh
@@ -2,9 +2,17 @@
 # exit on error
 set -o errexit
 
+# Instala dependencias del frontend
 npm install
 npm run build
 
-pipenv install
+# Instala pipenv (Render no lo trae por defecto)
+pip install pipenv
 
-pipenv run upgrade
+# Instala dependencias del backend
+pipenv install --deploy --ignore-pipfile
+
+# Ejecuta cualquier comando adicional (opcional)
+# pipenv run python manage.py migrate
+# pipenv run python manage.py collectstatic --noinput
+# pipenv run python app.py
