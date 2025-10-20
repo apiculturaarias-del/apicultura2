84d8af280b4ef2f1adbb4ce0ff91d08be8c9c326
apiculturaarias-del <apiculturaarias@gmail.com>
2025-10-20 18:27:36 +0000

    rerender

--- render_build.sh
+++ render_build.sh
@@ -9,6 +9,8 @@ pip install pipenv
 pipenv install --deploy --ignore-pipfile
 
 # 🚀 Asegurar que Python encuentre los módulos
+export PYTHONPATH=$PYTHONPATH:/opt/render/project/src/src
+
 # 📦 Crear tablas automáticamente si no existen
 echo "📦 Creando tablas en la base de datos si no existen..."
 pipenv run python -c "from api.app import app; from api.models import db; app.app_context().push(); db.create_all(); print('✅ Tablas creadas o ya existentes.')"
